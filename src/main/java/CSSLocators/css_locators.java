49263d01161341b941b9f48a850c68d83549026c
Rajkumar <raj31.vk@gmail.com>
2024-10-03 14:53:54 +0530

    Secont commit

--- src/main/java/CSSLocators/css_locators.java
+++ src/main/java/CSSLocators/css_locators.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class css_locators {
     public static void main(String[] args) {
+
+        System.out.println("Hello world");
         WebDriver driver = new ChromeDriver();
         driver.get("https://automationpanda.com/2019/01/15/web-element-locators-for-test-automation/");
         driver.manage().window().maximize();
